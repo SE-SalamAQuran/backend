8fbdd6ab799523e1788b1b4df658d194d1d69a0d
salam_quran <salamquraan789@gmail.com>
2020-10-30 12:37:01 +0300

    Adding the mock_data to DB

--- index.js
+++ index.js
@@ -6,43 +6,12 @@ const app = express();
 const ejs = require('ejs');
 const uri = 'mongodb://salam_quran:0TpCqcF1CSnhUwhu@cluster0-shard-00-00.lguwq.mongodb.net:27017,cluster0-shard-00-02.lguwq.mongodb.net:27017,cluster0-shard-00-01.lguwq.mongodb.net:27017/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 const port = process.env.PORT || 5000;
+const mock_data = require('./MOCK_DATA.json');
 app.set('view engine','ejs');
 
 
-const userSchema = mongoose.Schema({
-    fName: String,
-    lName: String,
-    ssn: Number,
-    email: String,
-    gender: String,
-    phone: String,
-    appointment: Array,
-    transactions: Array 
 
-});
 
-const UserModel = mongoose.model("UserModel",userSchema);
-const data = {
-id:404449605,
-first_name:"Salam",
-last_name:"Quran",
-email:"salamQuran@gmail.com",
-gender:"Male",
-address:"Ramallah",
-phone:"+970 (597) 156-276",
-appointments: [],
-transactions:[]
-
-};
-
-const newUser = new UserModel(data);
-newUser.save((err)=>{
-    if(err){
-        console.log("Error in saving document");
-    }else{
-        console.log("Data saved successfully");
-    }
-})
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -55,15 +24,52 @@ mongoose.connection.on('error', (err)=>{
     console.log("The error is:  " + err);
 });
 
+const userSchema = mongoose.Schema({
+    ssn: Number,
+    fName: String,
+    lName: String,
+    email: String,
+    gender: String,
+    address: String,
+    phone: String,
+    appointment: Array,
+    transactions: Array 
+
+});
+
+const UserModel = mongoose.model("UserModel",userSchema);
+
+
+// mock_data.forEach(user => {
+//     const newUsers = new UserModel(user);
+//     newUsers.save((err)=>{
+//         if(err){
+//             console.log("Error adding documents");
+//         }else{
+//             console.log("Mock_data saved in DB!");
+//         }
+//     });    
+// });
+
+//========================================================//
+
+// UserModel.deleteMany({} ,(err)=>{
+//     if(err){
+//         console.log("Can't Find data");
+//     }else{
+//         console.log("Deleted data successfully!");
+//     }
+// });
+
 app.get("/", (req,res)=>{
     UserModel.find({ })
-    .then((data)=>{
-        console.log('Data', data);
+    .then((mock_data)=>{
+        console.log('Data', mock_data);
     })
     .catch((err)=>{
         console.log('Error', err)
     });
-    res.json(data);
+    res.json(mock_data);
 });
 
 app.listen(port, ()=>{
