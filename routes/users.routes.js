bea42054a5663e87a75dbdd314289c75cd6075e2
salam_quran <salamquraan789@gmail.com>
2020-12-27 20:16:55 +0200

    Fixed logout

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router({ mergeParams: true });
 var userController = require("../controllers/users.controller");
 
 router.post("/login", userController.userLogin);
-router.post("/logout", userController.logout);
+router.delete("/logout", userController.logout);
 router.post("/register", userController.addNewUser);
 
 module.exports = router;
