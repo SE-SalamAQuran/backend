5414dbf3608da3901d20af779e7df5d16b424733
SE-SalamAQuran <salamquraan789@gmail.com>
2021-04-01 16:31:33 +0300

    sad

--- models/appointments.model.js
+++ models/appointments.model.js
@@ -1,55 +1,33 @@
 const { time } = require("faker");
 const mongoose = require("mongoose");
 
-
 const Schema = mongoose.Schema;
 
-const appointmentsSchema = new Schema({
-     
-    user:{
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-        required: true
+const appointmentsSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
     },
     property: {
-        type: Schema.Types.ObjectId,
-        ref: 'property'
+      type: Schema.Types.ObjectId,
+      ref: "property",
     },
     date: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
     },
     time: {
-        type: String,
+      type: String,
     },
-    place:{
-        type: String
+    place: {
+      type: String,
     },
-
-},{timestamps: true}
+  },
+  { timestamps: true }
 );
 
-const Appointments = mongoose.model("Appointments",appointmentsSchema);
-
-
-
-// data.forEach(app => {
-//     const newApp = new Appointments(app);
-//     newApp.save((err)=>{
-//         if(err){
-//             console.log("Error adding data", err);
-//         }else{
-//             console.log("Data added successfully");
-//         }
-//     });
-// });
-
-// Appointments.deleteMany({} ,(err)=>{
-//     if(err){
-//         console.log("Can't Find data");
-//     }else{
-//         console.log("Deleted data successfully!");
-//     }
-// });
+const Appointments = mongoose.model("Appointments", appointmentsSchema);
 
 module.exports = Appointments;
