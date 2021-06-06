dec4d949f7cc505159b024c59323fcb878363bd1
SE-SalamAQuran <salamquraan789@gmail.com>
2021-06-06 12:31:40 +0300

    disabled node_modules_cacge

--- index.js
+++ index.js
@@ -109,6 +109,10 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.get("/*", (req,res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
